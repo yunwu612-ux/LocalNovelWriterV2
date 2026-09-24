026561a720cf0e3a26f98cef5079cc2f4b087ec9
yunwu612-ux <yunwu612@gmail.com>
2026-09-24 21:52:09 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,5 @@
-android {
-    namespace = "com.localnovelwriter.app"
-    compileSdk = 35
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    defaultConfig {
-        applicationId = "com.localnovelwriter.app"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
-    }
-}
-
-dependencies {
-    // 原来的内容不要删
-}
-
-kotlin {
-    jvmToolchain(17)
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
